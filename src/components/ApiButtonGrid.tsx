674f32917422e3848412ef0d2fc5f5b90003eed8
suy0u <proton-coy3p@icloud.com>
2025-12-04 22:36:16 +0100

    refactor(component): changed size specification for grid

--- src/components/ApiButtonGrid.tsx
+++ src/components/ApiButtonGrid.tsx
@@ -3,7 +3,7 @@ import { pinkButton } from "../styles/buttons/buttons";
 
 export const ApiButtonGrid = () => (
   <Grid container spacing={2}>
-    <Grid xs={12} sm={6} md="auto">
+    <Grid size={{ xs: 12, sm: 6, md: "auto" }}>
       <Button sx={pinkButton} fullWidth>
         API Docs
       </Button>
--- src/components/HealthCheckGrid.tsx
+++ src/components/HealthCheckGrid.tsx
@@ -7,17 +7,17 @@ import {
 
 export const HealthCheckGrid = () => (
   <Grid container spacing={2}>
-    <Grid xs={12} sm={6} md="auto">
+    <Grid size={{ xs: 12, sm: 6, md: "auto" }}>
       <Button sx={purpleButton} fullWidth>
         App
       </Button>
     </Grid>
-    <Grid xs={12} sm={6} md="auto">
+    <Grid size={{ xs: 12, sm: 6, md: "auto" }}>
       <Button sx={mintButton} fullWidth>
         Redis
       </Button>
     </Grid>
-    <Grid xs={12} sm={6} md="auto">
+    <Grid size={{ xs: 12, sm: 6, md: "auto" }}>
       <Button sx={blueButton} fullWidth>
         DB
       </Button>
