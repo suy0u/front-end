9a23bb794fb54a3a7a56c86346ef9f4f45782c2c
suy0u <proton-coy3p@icloud.com>
2025-12-10 00:19:55 +0100

    feat(axios): configured axios instance and healthcheck endpoints

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
