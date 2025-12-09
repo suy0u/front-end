d913c358537167d5fecc9cde2adf699a4b8dc66c
suy0u <proton-coy3p@icloud.com>
2025-12-09 22:52:44 +0100

    feat(store): added store config and languageSlice

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "./i18n";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
