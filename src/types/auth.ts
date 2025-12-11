e822e2a291fbb1bf81689a99c90985f447749a92
suy0u <proton-coy3p@icloud.com>
2025-12-11 21:36:29 +0100

    fix(types): fixed loginResponse type

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,7 +1,13 @@
 export interface LoginResponse {
-  token: string;
-  userId: number;
-  email: string;
+  access_token: string;
+  access_token_expires_at: string;
+  refresh_token: string;
+  refresh_token_expires_at: string;
+  token_type: string;
+  user: {
+    id: number;
+    email: string;
+  };
 }
 
 export interface RegisterResponse {
