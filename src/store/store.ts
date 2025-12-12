99e7d4a34107099fc8639842d84220c36b328ca2
suy0u <proton-coy3p@icloud.com>
2025-12-12 15:24:38 +0100

    feat(store): added userSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import languageReducer from "./slices/languageSlice";
 import authReducer from "./slices/authSlice";
+import usersReducer from "./slices/userSlice.ts";
 
 export const store = configureStore({
   reducer: {
     language: languageReducer,
     auth: authReducer,
+    users: usersReducer,
   },
 });
 
