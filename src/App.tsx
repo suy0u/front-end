e9f8bbb5e99419be2edf610fe692f872ffcc819a
suy0u <proton-coy3p@icloud.com>
2025-12-12 16:06:06 +0100

    feat(app): added scroll up function when opening a new page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import AppRouter from "./router/AppRouter";
+import { ScrollToTop } from "./router/ScrollToTop";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Layout>
+        <ScrollToTop />
         <AppRouter />
       </Layout>
     </BrowserRouter>
