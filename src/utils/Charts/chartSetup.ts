6b72908fb981777dc84a1c486cdb973e3cd66f52
suy0u <proton-coy3p@icloud.com>
2025-12-22 00:58:19 +0100

    feat(deps): added chart.js

---
+++ src/utils/Charts/chartSetup.ts
@@ -0,0 +1,22 @@
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+} from "chart.js";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement
+);
