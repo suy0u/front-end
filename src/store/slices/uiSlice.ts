e5fae10b38cf20959a7b5b8292031f1020cbeb0c
suy0u <proton-coy3p@icloud.com>
2026-01-05 12:26:58 +0100

    refactor(store): deleted legacy code

--- src/store/slices/uiSlice.ts
+++ src/store/slices/uiSlice.ts
@@ -1,31 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// interface UiState {
-//   notificationsModalOpen: boolean;
-// }
-
-// const initialState: UiState = {
-//   notificationsModalOpen: false,
-// };
-
-// const uiSlice = createSlice({
-//   name: "ui",
-//   initialState,
-//   reducers: {
-//     openNotificationsModal(state) {
-//       state.notificationsModalOpen = true;
-//     },
-//     closeNotificationsModal(state) {
-//       state.notificationsModalOpen = false;
-//     },
-//   },
-// });
-
-// export const { openNotificationsModal, closeNotificationsModal } =
-//   uiSlice.actions;
-
-// export default uiSlice.reducer;
-
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { ListModalType } from "../../types/common";
 import type { MembershipState } from "../slices/membershipSlice";
